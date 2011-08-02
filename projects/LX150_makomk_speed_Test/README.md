131a4ee7d1243a191ad9aea394bf955138728bb9
progranism <fpgaminer@bitcoin-mining.com>
2011-08-01 23:44:33 -0700

    Added the LX150_makomk_speed_Test project.

---
+++ projects/LX150_makomk_speed_Test/README.md
@@ -0,0 +1,7 @@
+EXPERIMENTAL
+
+Based off LX150_makomk_Test
+
+Designing to achieve maximum clocking speed with a single fully unrolled core.
+The code has been stripped of its rolling parameters to help keep the code
+less cluttered, since this will be specifically optimized for fully unrolled speed.
---
+++ projects/LX150_makomk_speed_Test/hdl/fpgaminer_top.v
@@ -0,0 +1,114 @@
+/*
+*
+* Copyright (c) 2011 fpgaminer@bitcoin-mining.com
+*
+*
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+*/
+
+
+`timescale 1ns/1ps
+
+module fpgaminer_top (
+	input CLK_100MHZ
+);
+
+	//// 
+	reg [255:0] state = 0;
+	reg [127:0] data = 0;
+	reg [31:0] nonce = 32'h0;
+
+
+	//// PLL
+	wire hash_clk;
+`ifndef SIM
+	main_pll pll_blk (.CLK_IN1(CLK_100MHZ), .CLK_OUT1(hash_clk));
+`else
+	assign hash_clk = CLK_100MHZ;
+`endif
+
+
+	//// Hashers
+	wire [255:0] hash, hash2;
+
+	sha256_transform #(.NUM_ROUNDS(64)) uut (
+		.clk(hash_clk),
+		.rx_state(state),
+		.rx_input({384'h000002800000000000000000000000000000000000000000000000000000000000000000000000000000000080000000, data}),
+		.tx_hash(hash)
+	);
+	sha256_transform #(.NUM_ROUNDS(61)) uut2 (
+		.clk(hash_clk),
+		.rx_state(256'h5be0cd191f83d9ab9b05688c510e527fa54ff53a3c6ef372bb67ae856a09e667),
+		.rx_input({256'h0000010000000000000000000000000000000000000000000000000080000000, hash}),
+		.tx_hash(hash2)
+	);
+
+
+	//// Virtual Wire Control
+	reg [255:0] midstate_buf = 0, data_buf = 0;
+	wire [255:0] midstate_vw, data2_vw;
+
+`ifndef SIM
+	wire [35:0] control0, control1, control2;
+	chipscope_icon ICON_inst ( .CONTROL0(control0), .CONTROL1(control1), .CONTROL2(control2));
+	
+	chipscope_vio_tochip midstate_vw_blk ( .CONTROL(control0), .CLK(hash_clk), .SYNC_OUT(midstate_vw) );
+	chipscope_vio_tochip data_vw_blk ( .CONTROL(control1), .CLK(hash_clk), .SYNC_OUT(data2_vw) );
+`endif
+
+
+	//// Virtual Wire Output
+	reg [31:0] golden_nonce = 0;
+
+`ifndef SIM
+	chipscope_vio_fromchip golden_nonce_vw_blk ( .CONTROL(control2), .CLK(hash_clk), .SYNC_IN(golden_nonce) );
+`endif
+
+
+	//// Control Unit
+	reg is_golden_ticket = 1'b0;
+	wire [31:0] nonce_next;
+
+	assign nonce_next = nonce + 32'd1;
+	
+	always @ (posedge hash_clk)
+	begin
+		`ifdef SIM
+			//midstate_buf <= 256'h2b3f81261b3cfd001db436cfd4c8f3f9c7450c9a0d049bee71cba0ea2619c0b5;
+			//data_buf <= 256'h00000000000000000000000080000000_00000000_39f3001b6b7b8d4dc14bfc31;
+			//nonce <= 30411740;
+		`else
+			midstate_buf <= midstate_vw;
+			data_buf <= data2_vw;
+		`endif
+
+
+		// Give new data to the hasher
+		state <= midstate_buf;
+		data <= {nonce_next, data_buf[95:0]};
+		nonce <= nonce_next;
+
+
+		// Check to see if the last hash generated is valid.
+		is_golden_ticket <= (hash2[159:128] + 32'h5be0cd19 == 32'h00000000);
+		
+		if(is_golden_ticket)
+			golden_nonce <= nonce - 32'd130;
+	end
+
+endmodule
+
